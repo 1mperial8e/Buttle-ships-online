79d92e992461c321aa579e176d1628707002bcff
Aligator119 <vanja.irchak@gmail.com>
2014-04-28 16:01:42 +0300

    add Controllers

---
+++ Buttle ships online/ViewControllers/PrepareViewController/PrepareViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrepareViewController.h
+//  Buttle ships online
+//
+//  Created by Mac on 4/28/14.
+//  Copyright (c) 2014 mobilesoft365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrepareViewController : UIViewController
+
+@end
